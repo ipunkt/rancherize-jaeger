eeec781974549d72a6440c3458fabc32f2dafda1
svensp <sps@ipunkt.biz>
2019-05-15 09:50:19 +0200

    composer.json and empty provider

---
+++ plugin/JaegerProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+use Rancherize\Plugin\Provider;
+use Rancherize\Plugin\ProviderTrait;
+
+/**
+ * Class JaegerProvider
+ */
+class JaegerProvider implements Provider
+{
+    use ProviderTrait;
+
+    /**
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+    /**
+     */
+    public function boot()
+    {
+        // TODO: Implement boot() method.
+    }
+}
